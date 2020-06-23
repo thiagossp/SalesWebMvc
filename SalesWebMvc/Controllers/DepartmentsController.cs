6abe824937d1fe46f29f583408c57432d1ccf601
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-06-23 16:46:36 -0300

    First MVC - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Deparment> list = new List<Deparment>();
+            list.Add(new Deparment { Id = 1, Name = "Eletronics"});
+            list.Add(new Deparment { Id = 2, Name = "Fashion" });
+
+
+            return View(list);
+        }
+    }
+}
